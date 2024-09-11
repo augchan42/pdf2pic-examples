ae4dc82ec723402354a4c1833cbccec0c34c5089
Augustin Chan <aug@iterative.day>
2024-09-11 11:41:21 +0800

    support directory input

--- from-file-to-images.js
+++ from-file-to-images.js
@@ -1,24 +1,48 @@
 const { fromPath } = require("pdf2pic");
-const { mkdirsSync } = require("fs-extra");
+const { mkdirsSync, readdirSync } = require("fs-extra");
 const rimraf = require("rimraf");
+const path = require("path");
 
-module.exports = () => {
-  const specimen1 = "./files/specimen1.pdf";
-
+const convertPdfsToPngs = async () => {
+  const inputDirectory = "./files";
   const outputDirectory = "./output/from-file-to-images";
-  
+
+  // Clear and recreate the output directory
   rimraf.sync(outputDirectory);
-  
   mkdirsSync(outputDirectory);
-  
+
   const baseOptions = {
     width: 2550,
     height: 3300,
     density: 330,
-    savePath: outputDirectory
+    savePath: outputDirectory,
+    format: "png"
   };
-  
-  const convert = fromPath(specimen1, baseOptions);
-  
-  return convert.bulk(-1);
+
+  // Get all PDF files in the input directory
+  const pdfFiles = readdirSync(inputDirectory).filter(file => file.toLowerCase().endsWith('.pdf'));
+
+  // Process each PDF file
+  for (const file of pdfFiles) {
+    const inputPath = path.join(inputDirectory, file);
+    const outputBaseName = path.basename(file, '.pdf');
+    
+    const convert = fromPath(inputPath, baseOptions);
+
+    if (convert.bulk) {
+      const results = await convert.bulk(-1, false);
+      
+      // Rename the files
+      results.forEach((result, index) => {
+        const oldPath = result.path;
+        const newPath = path.join(outputDirectory, `${outputBaseName}.${index + 1}.png`);
+        if (oldPath && oldPath !== newPath) {
+          // Rename file if it exists and the name is different
+          require('fs').renameSync(oldPath, newPath);
+        }
+      });
+    }
+  }
 }
+
+module.exports = { convertPdfsToPngs };
--- index.js
+++ index.js
@@ -12,31 +12,34 @@ const fromBufferToImage = require("./from-buffer-to-image");
 const fromBufferToImagesSelected = require("./from-buffer-to-images-selected");
 const fromBufferToImages = require("./from-buffer-to-images");
 const fromFileToImage = require("./from-file-to-image");
-const fromFileToImages = require("./from-file-to-images");
+const fromFileToImages = require("./from-file-to-images.js");
 const fromFileToImagesSelected = require("./from-file-to-images-selected");
 const fromFileToBase64 = require("./from-file-to-base64");
 const fromFileToBase64Bulk = require("./from-file-to-base64-bulk");
 const fromFileToBase64BulkSelected = require("./from-file-to-base64-bulk-selected");
+const { convertPdfsToPngs } = require('./from-file-to-images');
+
 
 rimraf.sync("./output");
 
 Promise.all([
-  fromB64ToBase64BulkSelected(),
-  fromB64ToBase64Bulk(),
-  fromB64ToBase64(),
-  fromB64ToImage(),
-  fromB64ToImagesSelected(),
-  fromB64ToImages(),
-  fromBufferToBase64BulkSelected(),
-  fromBufferToBase64Bulk(),
-  fromBufferToBase64(),
-  fromBufferToImage(),
-  fromBufferToImagesSelected(),
-  fromBufferToImages(),
-  fromFileToImage(),
-  fromFileToImages(),
-  fromFileToImagesSelected(),
-  fromFileToBase64(),
-  fromFileToBase64Bulk(),
-  fromFileToBase64BulkSelected()
+  // fromB64ToBase64BulkSelected(),
+  // fromB64ToBase64Bulk(),
+  // fromB64ToBase64(),
+  // fromB64ToImage(),
+  // fromB64ToImagesSelected(),
+  // fromB64ToImages(),
+  // fromBufferToBase64BulkSelected(),
+  // fromBufferToBase64Bulk(),
+  // fromBufferToBase64(),
+  // fromBufferToImage(),
+  // fromBufferToImagesSelected(),
+  // fromBufferToImages(),
+  // fromFileToImage(),
+  // fromFileToImages(),
+  convertPdfsToPngs(),
+  // fromFileToImagesSelected(),
+  // fromFileToBase64(),
+  // fromFileToBase64Bulk(),
+  // fromFileToBase64BulkSelected()
 ]);
